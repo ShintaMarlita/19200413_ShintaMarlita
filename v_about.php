96b2a296a62c1c1315a9d906001e0d3ab8c85b1c
ShintaMarlita <90750699+ShintaMarlita@users.noreply.github.com>
2021-10-11 01:10:51 +0700

    web rental buku
    Screenshot 171 = Tampilan Login
    Screenshot 172 = Tampilan Register

---
+++ v_about.php
@@ -0,0 +1,21 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Shinta </li> 
+        <li>Nama Belakang : Marlita</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Cilobak 4</li> 
+    </ul> 
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Berenang</li> 
+        <li>Bulu Tangkis</li> 
+    </ul> 
+</section>
